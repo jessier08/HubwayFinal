66440ca42dcf620ba6ece51d80ac0ec119d4e8f9
jessier08 <jessierichards@Jessies-MacBook-Pro-2.local>
2016-04-27 15:35:00 -0400

    fixed station list appending, line math almost there, took away extra graphs

--- scripts/scriptTest.js
+++ scripts/scriptTest.js
@@ -17,16 +17,34 @@ var map = d3.select('#map')
     .attr('width', mapW)
     .attr('height', mapH);
 
+// margins and div selection for map
+var linesW = d3.select('#lineBox').node().clientWidth,
+    linesH = d3.select('#lineBox').node().clientHeight;
+    
+var stationLines = d3.select('#lineBox')
+    .append('svg')
+    .attr('width', linesW)
+    .attr('height', linesH);
+
+// margins and div selection for gender type graph
+//var genderM = {t:0,r:0,b:0,l:0},
+//    genderW = d3.select('#genderType').node().clientWidth,
+//    genderH = d3.select('#genderType').node().clientHeight;
+//
+//var genderTypePlot = d3.select('#genderType').append('svg')
+//    .attr({width: userW, height: userH});
+//
+//var genderScaleY = d3.scale.linear().range([genderH,0]);
+
 // margins and div selection for user type graph
-var userM = {t:0,r:0,b:0,l:0},
-    userW = d3.select('#userType').node().clientWidth,
-    userH = d3.select('#userType').node().clientHeight;
-
-var userTypePlot = d3.select('#userType').append('svg')
-    .attr({width: userW, height: userH});
-
-var userScaleX = d3.scale.ordinal(),
-    userScaleY = d3.scale.linear().range([userH/1.5,0]);
+//var userM = {t:0,r:0,b:0,l:0},
+//    userW = d3.select('#userType').node().clientWidth,
+//    userH = d3.select('#userType').node().clientHeight;
+//
+//var userTypePlot = d3.select('#userType').append('svg')
+//    .attr({width: userW, height: userH});
+//
+//var userScaleY = d3.scale.linear().range([userH/1.5,20]);
 
 // create dispatcher 
 var globalDispatcher = d3.dispatch('changetimeextent');
@@ -35,16 +53,18 @@ var globalDispatcher = d3.dispatch('changetimeextent');
 var stationsName = d3.map(),
     stationsSpot = d3.map();
 
-console.log(stationsName);
-console.log(stationsSpot);
+//console.log(stationsName);
+//console.log(stationsSpot);
 
 // LOAD DATA
 queue()
     .defer(d3.csv,'../data/hubway_trips_reduced.csv',parse)
-    .defer(d3.csv,'../data/hubway_stations.csv',parseStations)
+    .defer(d3.csv,'../data/hubway_stations_namesEdit.csv',parseStations)
     .await(dataLoaded);
 
 function dataLoaded(err,trips,stations){
+    
+    //console.log(trips);
     
     // tells map to look for start and end station
     trips.forEach(function(d){
@@ -52,10 +72,10 @@ function dataLoaded(err,trips,stations){
         d.endStationName = stationsName.get(d.endStation);
     })
     
-//    trips.forEach(function(d){
-//        d.startStationSpot = stationsSpot.get(d.startStation);
-//        d.endStationsSpot = stationsSpot.get(d.endStation);
-//    })
+    trips.forEach(function(d){
+        d.stationLng = stationsSpot.get(d.startStation);
+        d.stationLat = stationsSpot.get(d.endStation);
+    })
     
 ////// DATA FILTERING / SORTING //////
     // group by start time
@@ -68,9 +88,7 @@ function dataLoaded(err,trips,stations){
     globalDispatcher.on('changetimeextent',function(extent){
         d3.select('.ranges').select('.start-date').html(extent[0].getMonth()+1+'/'+extent[0].getDate()+'/'+extent[0].getFullYear()+' - ');
         d3.select('.ranges').select('.end-date').html(extent[1].getMonth()+1+'/'+extent[1].getDate()+'/'+extent[1].getFullYear());
-        
-
-    
+         
         // filter selected trips, return the amount of trips to DOM  
         tripsByStartTime.filterRange(extent);
         
@@ -120,6 +138,7 @@ function dataLoaded(err,trips,stations){
             tripsByStartStation = cf.dimension(function(d){return d.startStationName}),
             tripsByStartStation.filter(topStationsArray);
         //console.log(tripsByStartStation.top(10));
+        
         // start filtering data by end station
         var tripsByEndStation = cf.dimension(function(d){return d.endStationName});
 
@@ -139,120 +158,228 @@ function dataLoaded(err,trips,stations){
         }
         
 ////// PLOTTING POPULAR STATION TEXT //////
-        if(d3.select('#start')){
-            d3.select('#start').remove();  
-            d3.select('#end').remove();
-        }
-//        if(d3.select('#end')){
-//            d3.select('#end').remove();  
+        // THE OLD WAY WITH TSPAN
+//        if(d3.select('#start')){
+//            d3.select('#start').remove();  
+//            d3.select('#end').remove();
 //        }
-//        
+
         // append svg to print start stations to DOM
-        var startSvg = d3.select('#startBox')
-            .append('svg')
-            .attr('id','start')
-            .attr('height',350);
-
-        var startText = startSvg.append('text');
-
-        startText.selectAll('tspan')
-            .data(topStationsArray)
-            .enter()
-            .append('tspan')
-            .attr('x',d3.select('#startBox').node().clientWidth)
-            .attr('dy','2.75em')
-            .attr('id',function(d,i){return i;})
+//        var startSvg = d3.select('#startBox')
+//            .append('svg')
+//            .attr('id','start')
+//            .attr('height',350);
+//
+//        var startText = startSvg.append('text');
+//            
+//        startText.selectAll('tspan')
+//            .data(topStationsArray)
+//            .enter()
+//            .append('tspan')
+//            .on('click', function(){
+//                var textLocation = d3.select(this).node().getBoundingClientRect().right;                         console.log(textLocation);
+//            })
+//            .attr('id',function(d,i){return i;})
+//            .attr('x',d3.select('#startBox').node().clientWidth)
+//            .attr('dy','2.75em')
+//            .text(function(d){return d;})
+//            .attr('class','stationText')
+//            .attr('text-anchor','end')
+//            .on('mouseover', function(){
+//                d3.select(this).attr('class','green');
+//                //console.log(this.id);
+//            })
+//            .on('mouseout', function(){
+//                d3.select(this).attr('class','stationText');
+//                //console.log(this.id);
+//            });
+//        
+//         if(d3.select('#startBox')){
+//            d3.select('#startBox').exit().remove();  
+//            d3.select('#endBox').exit().remove();
+//        }
+////// PLOTTING POPULAR STATION TEXT //////
+        // A NEW WAY WITH DIV TEXT
+        if(d3.select('.startText')){
+            d3.select('.startText').remove();    
+        }
+        if(d3.select('.endText')){
+            d3.select('.endText').remove();    
+        }
+        
+        
+        var startText = d3.select('#startBox')
+            .selectAll('.startText')
+            .data(topStationsArray);
+                    
+        var x,y;
+        
+        startText.enter()
+            .append('div')
             .text(function(d){return d;})
-            .attr('text-anchor','end')
-            .attr('class','stationText')
+            .attr('class','stationText startText')
             .on('mouseover', function(){
                 d3.select(this).attr('class','green');
-                //console.log(this.id);
             })
             .on('mouseout', function(){
-                d3.select(this).attr('class','stationText');
-                //console.log(this.id);
+                d3.select(this).attr('class','startText stationText');
             })
-            ;
+            .on('click', function(){
+                var xy = [d3.select(this).node().getBoundingClientRect().right, d3.select(this).node().getBoundingClientRect().top];
+               
+                console.log(xy);
+            
+                stationLines
+                    .append('line')
+                    .attr('x1',0)
+                    .attr('y1',xy[1]-107.5)
+                    .attr('x2',xy[0]+100)
+                    .attr('y2',xy[1]+100)
+                    .attr('stroke','black')
+                    .attr('stroke-width',2);     
+            });
+        
 
-        //write endstations text
-        var endSvg = d3.select('#endBox')
-            .append('svg')
-            .attr('id','end')
-            .attr('height',350);
-
-        var endText = endSvg.append('text');
-
-        endText.selectAll('tspan')
+        var endText = d3.select('#endBox')
+            .selectAll('.endText')
             .data(topEndStationsArray)
-            .enter()
-            .append('tspan')
-            .attr('id',function(d,i){return i;})
-            .attr('x',0)
-            .attr('dy','2.75em')
+        
+        endText.enter()
+            .append('div')
             .text(function(d){return d;})
-            .attr('class','stationText');
+            .attr('class','stationText endText');
         
 
-    ////// USER TYPE GRAPH //////
-        var crossF = crossfilter(topStations),
-            tripsByUserType = cf.dimension(function(d){return d.userType});
+
+//    ////// USER TYPE GRAPH //////
+//        var crossF = crossfilter(topStations),
+//            tripsByUserType = cf.dimension(function(d){return d.userType});
+//        
+//        var casualUser = tripsByUserType.filter('Casual').top(Infinity),
+//            numCasualUser = casualUser.length;
+//    
+//        tripsByUserType.filter(null);
+//    
+//        var regUser = tripsByUserType.filter('Registered').top(Infinity),
+//            numRegUser = regUser.length;
+//   
+//        var userTypes = [numCasualUser,numRegUser];
+//        
+//        //console.log(userTypes);
+//        //console.log(userTypes[0],userTypes[1]);
+//    
+//        userScaleY.domain([0,115000]);
+//   
+//        var casual = userTypePlot.selectAll('.casual')
+//            .data(userTypes)
+//        
+//        casual.enter().append('rect')
+//            .attr('class','casual bars')
+//            .attr('transform','translate(15,0)');
+//        
+//        casual
+//            .transition()
+//            .attr('x', userW/4)
+//            .attr('y', userScaleY(userTypes[0]))
+//            .attr('width', 10)
+//            .attr('height', userH - userScaleY(userTypes[0]));
+//        
+//        var reg = userTypePlot.selectAll('.reg')
+//            .data(userTypes);
+//        
+//        reg.enter().append('rect')
+//            .attr('class','reg bars')
+//            .attr('transform','translate(10,0)');
+//        
+//        reg
+//            .transition()
+//            .attr('x', userW-userW/4)
+//            .attr('y', userScaleY(userTypes[1]))
+//            .attr('width', 10)
+//            .attr('height', userH - userScaleY(userTypes[1]));
+//        
         
-        var casualUser = tripsByUserType.filter('Casual').top(Infinity),
-            numCasualUser = casualUser.length;
-    
-        tripsByUserType.filter(null);
-    
-        var regUser = tripsByUserType.filter('Registered').top(Infinity),
-            numRegUser = regUser.length;
-   
-        var userTypes = [numCasualUser,numRegUser];
         
-        //console.log(userTypes);
-        //console.log(userTypes[0],userTypes[1]);
-    
-        userScaleY.domain([0,115000]);
-   
-        var casual = userTypePlot.selectAll('.casual')
-            .data(userTypes)
+        if(d3.select('.circle')){
+            d3.select('.circle').remove();  
+        }
+          
+        //console.log(topStations);           
+        var stationDots = map.append('g').attr('class','circle');
+        //console.log(topStations);
         
-        casual.enter().append('rect')
-            .attr('class','casual bars');
+        stationDots.selectAll('.circle')
+            .data(stations)
+            .enter()
+            .append('circle')
+            //.filter(function(d){if stationName is in topStations, return d.startStationName})
+            .attr('r',3)
+             .attr('cx', function(d){
+                var xy = albersProjection(d.lngLat);
+                return xy[0]})
+            .attr('cy', function(d){
+                var xy = albersProjection(d.lngLat);
+                return xy[1]});  
+
+        // attempt at gender freq graph
+//        var newFilter = crossfilter(topStations),
+//            tripsByGender = newFilter.dimension(function(d){return d.gender});
+//        
+//        var maleUser = tripsByGender.filter('Male').top(Infinity),
+//            numMaleUser = maleUser.length;
+//    
+//        tripsByGender.filter(null);
+//    
+//        var femaleUser = tripsByGender.filter('Female').top(Infinity),
+//            numFemaleUser = femaleUser.length;
+//   
+//        var userGender = [numMaleUser,numFemaleUser];
+//        
+//        console.log(userGender);
+//        
+//        genderScaleY.domain([0,60000]);
+//   
+//        var male = genderTypePlot.selectAll('.male')
+//            .data(userGender)
+//        
+//        male.enter().append('rect')
+//            .attr('class','male bars');
+//        
+//        male
+//            .transition()
+//            .attr('x', genderW/4)
+//            .attr('y', genderScaleY(userGender[0]))
+//            .attr('width', 10)
+//            .attr('height', genderScaleY(userGender[0]))
+//            .attr('transform','translate(-5,0)');
+//        
+//        var female = genderTypePlot.selectAll('.female')
+//            .data(userGender);
+//        
+//        female.enter().append('rect')
+//            .attr('class','female bars');
+//        
+//        female
+//            .transition()
+//            .attr('x', genderW-genderW/4)
+//            .attr('y', genderScaleY(userGender[1]))
+//            .attr('width', 10)
+//            .attr('height', genderH - genderScaleY(userGender[1]))
+//            .attr('transform','translate(-5,0)');
         
-        casual
-            .transition()
-            .attr('x', userW/4)
-            .attr('y', userScaleY(userTypes[0]))
-            .attr('width', 10)
-            .attr('height', userH - userScaleY(userTypes[0]))
-            .attr('transform','translate(-5,0)');
-        
-        var reg = userTypePlot.selectAll('.reg')
-            .data(userTypes);
-        
-        reg.enter().append('rect')
-            .attr('class','reg bars');
-        
-        reg
-            .transition()
-            .attr('x', userW-userW/4)
-            .attr('y', userScaleY(userTypes[1]))
-            .attr('width', 10)
-            .attr('height', userH - userScaleY(userTypes[1]))
-            .attr('transform','translate(-5,0)');
   
     });
     // end global dispatch 
 
 ////// ALL TRIPS HISTOGRAM WITH BRUSH //////
     // create inputs for start-date histogram
-    var timeExtent = [new Date(2011,7,20),new Date(2013,7,20)],
+    var timeExtent = [new Date(2011,5,20),new Date(2013,12,30)],
         binSize = d3.time.day,
         bins = d3.time.day.range(timeExtent[0],timeExtent[1]);
 
     // scales and axis
     var scaleX = d3.time.scale().domain(timeExtent).range([0,w]),
-        scaleY = d3.scale.linear().range([h,0]),
+        scaleY = d3.scale.linear().range([h,60]),
         axisX = d3.svg.axis()
             .scale(scaleX)
             .orient('bottom')
@@ -261,8 +388,8 @@ function dataLoaded(err,trips,stations){
                 return (tick.getMonth()+1) + ' / ' + tick.getFullYear()  ;   
                 return tick.getMonth()+1;
             })
-    d3.selectAll('.t')
-    // create histogram function
+    
+    // create histogram layout
     var layout = d3.layout.histogram()
         .value(function(d){return d.startTime})
         .range(timeExtent)
@@ -287,7 +414,9 @@ function dataLoaded(err,trips,stations){
     plot.append('g').attr('class','axis axis-x')
         .attr('transform','translate(0,'+h+')')
         .call(axisX);
-    d3.selectAll('.tick > text').attr('class','axisFont')
+    
+    d3.selectAll('.tick > text').attr('class','axisFont');
+    
     // implement brush
     var brush = d3.svg.brush()
         .x(scaleX)
@@ -333,22 +462,15 @@ function dataLoaded(err,trips,stations){
                 .enter()
                 .append('path')
                 .attr('d', geoPath);
+    
+    // LOAD VISUALIZATION FOR ALL TRIPS WITH PAGE LOAD
+    var init_timeExtent = [new Date(2011,7,20),new Date(2013,7,20)]
+
+    $(document).ready(function() {
+        globalDispatcher.changetimeextent(init_timeExtent)
+    });
         
-            var stationDots = map.selectAll('.circle')
-                .data(stations);
-            
-            stationDots.enter().append('circle')
-                .attr('class','circle');
-            
-            stationDots
-                .filter(function(d){return d.stationsName})
-                .attr('r',3)
-                 .attr('cx', function(d){
-                    var xy = albersProjection(d.lngLat);
-                    return xy[0]})
-                .attr('cy', function(d){
-                    var xy = albersProjection(d.lngLat);
-                    return xy[1]})   
+           
 
     
 
@@ -367,6 +489,7 @@ function dataLoaded(err,trips,stations){
 
 // PARSING FUNCTIONS
 function parse(d){
+    
     if(+d.duration<0) return;
 
     return {
@@ -375,8 +498,9 @@ function parse(d){
         endTime: parseDate(d.end_date),
         startStation: d.strt_statn,
         endStation: d.end_statn,
-        userType: d.subsc_type
-        //gender:d.gender,//can read string d.gender=="" ? "none" :d.gender
+        userType: d.subsc_type,
+        gender: d.gender,
+        //startHour: parseTime(d.start_date)
         //birthDate:+d.birth_date,
         //data:d3.map
     };
@@ -387,16 +511,19 @@ function parseDate(date){
 
     return new Date(+day[2],+day[0]-1, +day[1], +time[0], +time[1]);
 }
+function parseTime(){
+    //return d3.time.day(d.startTime);
+}
+
 function parseStations(d){
     //setting values for earlier defined map 
-    stationsName.set(d.id,d.station); 
-    
-    stationsSpot.set([+d.lng,+d.lat], d.station);
+    stationsName.set(d.id,d.station);
+    stationsSpot.set(d.id,+d.lng,+d.lat);
     
     return {
         lngLat: [+d.lng,+d.lat],
+        //lat: +d.lat,
         stationName: d.station,
-        id: d.id,
-        stationName:d.station
+        id: d.id
     }; 
 }
